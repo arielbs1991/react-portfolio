e38852e2b0a9c809d38786aa7bf5142a721a11de
Ariel <arielbs1991@gmail.com>
2020-08-16 18:30:57 -0700

    filled out portfolio card. About to test. Fingers crossed.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Grid, Card, Typography } from '@material-ui/core';
+import ProjectCard from '../ProjectCard';
 
 
 const Portfolio = () => {
@@ -15,22 +16,58 @@ const Portfolio = () => {
                         <Typography variant="h3">My Portfolio</Typography>
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                        <ProjectCard
+                            title={project[0].title}
+                            image={project[0].image}
+                            github={project[0].github}
+                            deployed={project[0].deployed}
+                            description={project[0].description}
+                        />
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                    <ProjectCard
+                            title={project[1].title}
+                            image={project[1].image}
+                            github={project[1].github}
+                            deployed={project[1].deployed}
+                            description={project[1].description}
+                        />
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                    <ProjectCard
+                            title={project[2].title}
+                            image={project[2].image}
+                            github={project[2].github}
+                            deployed={project[2].deployed}
+                            description={project[2].description}
+                        />
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                    <ProjectCard
+                            title={project[3].title}
+                            image={project[3].image}
+                            github={project[3].github}
+                            deployed={project[3].deployed}
+                            description={project[3].description}
+                        />
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                    <ProjectCard
+                            title={project[4].title}
+                            image={project[4].image}
+                            github={project[4].github}
+                            deployed={project[4].deployed}
+                            description={project[4].description}
+                        />
                     </Grid>
                     <Grid item xs={12} md={6} lg={4}>
-
+                    <ProjectCard
+                            title={project[5].title}
+                            image={project[5].image}
+                            github={project[5].github}
+                            deployed={project[5].deployed}
+                            description={project[5].description}
+                        />
                     </Grid>
                 </Grid>
             </Container>
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import BurgerBuilders from '../../images/burger-builders.png';
 import DayScheduler from '../../images/day-scheduler.png';
 import NoteTaker from '../../images/note-taker.png';
@@ -8,7 +8,7 @@ import RNGPassword from '../../images/rng-password.png';
 import Wavel from '../../images/wavel.png';
 import WineKey from '../../images/wine-key.png';
 
-const MediaCard = () => {
+const ProjectCard = (props) => {
     const useStyles = makeStyles({
         root: {
             maxWidth: 345,
@@ -25,29 +25,28 @@ const MediaCard = () => {
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                />
+                    image={props.image}
+                    title={props.title}
+                ><a href={props.deployed} target="_blank"></a></CardMedia>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-          </Typography>
+                    <Typography gutterBottom variant="h5" component="h2"><a href={props.deployed} target="_blank">
+                        {props.title}
+                    </a></Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-          </Typography>
+                        {props.description}
+                    </Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                    Share
-        </Button>
-                <Button size="small" color="primary">
-                    Learn More
-        </Button>
+                <Button size="small" color="primary"><a href={props.github} target="_blank">
+                    Project Github
+                </a></Button>
+                <Button size="small" color="primary"><a href={props.deployed} target="_blank">
+                    Deployed Site
+        </a></Button>
             </CardActions>
         </Card>
     );
 }
 
-export default MediaCard;
+export default ProjectCard;
